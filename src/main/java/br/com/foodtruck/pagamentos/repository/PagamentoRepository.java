535fc115c9387f1ac30d87df72fe9235cb6d9537
doni-zete <donizete112@gmail.com>
2024-04-30 07:17:07 -0300

    refactor: Alterando diretorios

--- src/main/java/br/com/foodtruck/pagamentos/repository/PagamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.foodtruck.pagamentos.repository;
-
-import br.com.foodtruck.pagamentos.model.Pagamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
-}
--- src/main/java/br/com/foodtruck/pagamentos/util/Status.java
+++
@@ -1,7 +0,0 @@
-package br.com.foodtruck.pagamentos.util;
-
-public enum Status {
-  CRIADO,
-  CONFIRMADO,
-  CANCELADO
-}
