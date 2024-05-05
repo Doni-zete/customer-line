730ad8cc4caf1b9101727eb827e255b172181ee7
doni-zete <donizete112@gmail.com>
2024-05-05 16:39:43 -0300

    feat: criando o pagamentos.detalhes-pedido

--- pedidos/src/main/java/br/com/food/pedidos/amqp/PedidoAMQPConfiguration.java
+++ pedidos/src/main/java/br/com/food/pedidos/amqp/PedidoAMQPConfiguration.java
@@ -1,5 +1,9 @@
 package br.com.food.pedidos.amqp;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -9,6 +13,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PedidoAMQPConfiguration {
+
+  //Comentei porque não vai ser criado mais a a fila, e sim uma exchange para enviar para dois MS
+//    @Bean
+//    public Queue criaFila() {
+////        return new Queue("pagamento.concluido",false);
+//        return QueueBuilder.nonDurable("pagamento.concluido").build();
+//    }
   @Bean
   public Jackson2JsonMessageConverter messageConverter() {
     return new Jackson2JsonMessageConverter();
@@ -21,4 +32,36 @@ public class PedidoAMQPConfiguration {
     rabbitTemplate.setMessageConverter(messageConverter);
     return rabbitTemplate;
   }
+
+  @Bean
+  public Queue filaDetalhesPedido() {
+    return QueueBuilder
+      .nonDurable("pagamentos.detalhes-pedido")
+      .build();
+  }
+
+  @Bean
+  public FanoutExchange fanoutExchange() {
+    return ExchangeBuilder
+      .fanoutExchange("pagamentos.ex")
+      .build();
+  }
+
+  @Bean
+  public Binding bindPagamentoPedido(FanoutExchange fanoutExchange) {
+    return BindingBuilder
+      .bind(filaDetalhesPedido())
+      .to(fanoutExchange());
+  }
+
+  @Bean
+  public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
+    return new RabbitAdmin(conn);
+  }
+
+  @Bean
+  public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin){
+    return event -> rabbitAdmin.initialize();
+  }
+
 }
